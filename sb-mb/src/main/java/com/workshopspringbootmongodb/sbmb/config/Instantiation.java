abe812f451e40fe2c25fe1ed2d57e6b847ebd4fa
Giulia Forlin <giuliaforlin2009@gmail.com>
2023-03-12 17:46:07 -0300

    The user's authored content

--- sb-mb/src/main/java/com/workshopspringbootmongodb/sbmb/config/Instantiation.java
+++ sb-mb/src/main/java/com/workshopspringbootmongodb/sbmb/config/Instantiation.java
@@ -49,7 +49,7 @@ public class Instantiation implements CommandLineRunner {
 
         postReposiroty.saveAll(Arrays.asList(post1, post2));
 
-//       maria.getPosts().addAll(Arrays.asList(post1, post2));
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
         userReposiroty.save(maria);
     }
 }
