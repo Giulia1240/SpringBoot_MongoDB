2d87e0200c262009932ccf70afcc3b484da1d37c
Giulia Forlin <giuliaforlin2009@gmail.com>
2023-03-12 18:24:54 -0300

    To add comments to the post

--- sb-mb/src/main/java/com/workshopspringbootmongodb/sbmb/config/Instantiation.java
+++ sb-mb/src/main/java/com/workshopspringbootmongodb/sbmb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.workshopspringbootmongodb.sbmb.config;
 import com.workshopspringbootmongodb.sbmb.domain.Post;
 import com.workshopspringbootmongodb.sbmb.domain.User;
 import com.workshopspringbootmongodb.sbmb.dto.AuthorDTO;
+import com.workshopspringbootmongodb.sbmb.dto.CommentDTO;
 import com.workshopspringbootmongodb.sbmb.repository.PostRepository;
 import com.workshopspringbootmongodb.sbmb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,12 +41,12 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-//        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
-//        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
-//        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 
-//        post1.getComments().addAll(Arrays.asList(c1, c2));
-//        post2.getComments().addAll(Arrays.asList(c3));
+        post1.getComment().addAll(Arrays.asList(c1, c2));
+        post2.getComment().addAll(Arrays.asList(c3));
 
         postReposiroty.saveAll(Arrays.asList(post1, post2));
 
--- sb-mb/src/main/java/com/workshopspringbootmongodb/sbmb/domain/Post.java
+++ sb-mb/src/main/java/com/workshopspringbootmongodb/sbmb/domain/Post.java
@@ -1,11 +1,15 @@
 package com.workshopspringbootmongodb.sbmb.domain;
 
 import com.workshopspringbootmongodb.sbmb.dto.AuthorDTO;
+import com.workshopspringbootmongodb.sbmb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 @Document
 public class Post implements Serializable {
@@ -19,6 +23,7 @@ public class Post implements Serializable {
 
      private AuthorDTO author;
 
+     private List<CommentDTO> comment= new ArrayList<>();
     public Post() {
     }
 
@@ -68,6 +73,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComment() {
+        return comment;
+    }
+
+    public void setComment(List<CommentDTO> comment) {
+        this.comment = comment;
     }
 
     @Override
